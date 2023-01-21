3e6ca03f2735cc93af6366c1e314a3294a401eb7
Sumit <kushsumit27@outlook.com>
2023-01-21 11:56:47 +0530

    add ejs view engine

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,8 @@ const router = express.Router();
 const homeController = require('../controllers/home_controller');
 console.log('router loaded')
 
-router.get('/', homeController.home)
+router.get('/', homeController.home);
+
+router.use('/users', require('./users'));
 module.exports = router;
 
